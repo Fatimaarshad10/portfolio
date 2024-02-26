c75fed5e1b71e7402e81eaa4133f0ddaea39b80d
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-02-26 22:14:45 +0500

    Navbar complete 26/02/2024 - 1

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,13 @@
+const ContactPage = () => {
+  return (
+    <div className="w-28 h-24 bg-black text-white" >ContactPage
+      <div class="flex flex-col hover:flex-row">
+        <div className="bg-orange-400 w-52 h-40">01</div>
+        <div className="bg-gray-500 w-52 h-40 ">02</div>
+        <div className="bg-red-700 w-52 h-40" >03</div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactPage
