647f01c3eda919d0bd10d2854fbde0413588c812
Fatimaarshad10 <fatimaarshad091@gmail.com>
2024-03-05 23:57:28 +0500

    About page  05/03/2024 - 1

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { motion } from "framer-motion";
 import Image from "next/image";
 
@@ -7,152 +6,146 @@ const AboutPage = () => {
   return (
     <motion.div className="h-full" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
       {/* CONTAINER */}
-      <div className=" ">
+      <div className="h-full overflow-y-auto lg-flex ">
         {/* TEXT CONTAINER */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-30 flex flex-col gap-24 md:gap-32 lg:gap-38 xl:gap-39">
-          {/* BIOGRAPHY CONTAINER */}
-          <div className=" flex flex-col gap-12 justify-center">
-            {/* BIOGRAPHY TITLE */}
-
-            <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
-            {/* BIOGRAPHY DESC */}
-
-            <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quis fugit nihil explicabo, et maiores id labore cumque facere quo ipsam modi velit omnis! Consequuntur laboriosam beatae dolorum nostrum dolore?</p>
-            {/* BIOGRAPHY QUOTE */}
-
-            <span className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
-            {/* BIOGRAPHY SIGN */}
-            <Image src='/svg.svg' alt="svg" width={50} height={50} className="self-end" />
-            <div className=""></div>
-            {/* BIOGRAPHY SVG */}
-
-          </div>
-          {/* SKILLS CONTAINER */}
-          <div className="flex flex-col gap-12 justify-center">
-            {/* SKILLS TITLE */}
-            <h1 className="font-bold text-2xl">SKILLS</h1>
-            {/* SKILLS LIST */}
-            <div className="flex flex-wrap gap-4">
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>
-              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
-                JavaScript
-              </div>
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-30 flex flex-col gap-24 md:gap-32 lg:gap-38 xl:gap-39 ">
+          <div className="flex  flex-row gap-40">
+            <div className="flex flex-col gap-12 justify-center">
+              <h1 className="font-bold text-2xl">BIOGRAPHY</h1>
+              <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+                Adipisci quis fugit nihil explicabo, et maiores id <br />labore cumque facere quo ipsam modi<br /></p>
+              <span className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
             </div>
-            {/* SKILLS SCROLL SVG */}
-            <div className=""></div>
+            <Image src='/18383-removebg-preview.png' alt="svg" width={300} height={200} className="hidden lg:block"/>
           </div>
-          {/* EXPERIENCE CONTAINER */}
-          <div className=" flex flex-col gap-12 justify-center pb-48">
-            {/* EXPERIENCE TITLE */}
-            <h1 className="font-bold text-2xl">EXPERIENCE </h1>
-            {/* EXPERIENCE LIST */}
-            <div className="">
-              {/* EXPERIENCE LIST ITEM */}
-              <div className="">
-                {/* LEFT */}
-                <div className="">
-                {/* JOB TITLE */}
-<div className="">Backend Developer</div>
-                {/* JOB DESC */}
-<div className="">My current employment. Way better than the position before!</div>
-                {/* JOB DATE */}
-<div className="">2023 - Present</div>
-                {/* JOB COMPANY */}
-<div className="">NAXTECH</div>
-                </div>
+ {/* SKILLS CONTAINER */}
+ <div className="flex flex-col gap-12 justify-center">
+    {/* SKILLS TITLE */}
+    <h1 className="font-bold text-2xl">SKILLS</h1>
+    {/* SKILLS LIST */}
+    <div className="flex flex-wrap gap-4">
+      <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>
+      <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>
+      <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>    <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black ">
+        JavaScript
+      </div>
+    </div>
+ 
+  </div>
+  {/* EXPERIENCE CONTAINER */}
+  <div className=" flex flex-col gap-12 justify-center pb-48">
+    {/* EXPERIENCE TITLE */}
+    <h1 className="font-bold text-2xl">EXPERIENCE </h1>
+    {/* EXPERIENCE LIST */}
+    <div className="">
+      {/* EXPERIENCE LIST ITEM */}
+      <div className="flex justify-between h-48">
+        {/* LEFT */}
+        <div className="w-1/3 ">
+          {/* JOB TITLE */}
+          <div className="p-3 bg-white font-semibold rounded-b-lg rounded-s-lg">Backend Developer</div>
+          {/* JOB DESC */}
+          <div className="p-3 text-sm italic">My current employment. Way better than the position before!</div>
+          {/* JOB DATE */}
+          <div className="p-3 text-red-400 text-sm font-semibold">2023 - Present</div>
+          {/* JOB COMPANY */}
+          <div className="p-1 bg-white rounded  text-sm font-semibold w-fit">NAXTECH</div>
+        </div>
 
-                {/* CENTER */}
-                <div className="">
-              {/* LINE */}
-              <div className="">
-                {/* LINE CIRCLE */}
-              </div>
-                </div>
-                {/* RIGHT */}
-<div className=""></div>
-              </div>
-                {/* EXPERIENCE LIST ITEM */}
-                <div className="">
-                {/* LEFT */}
-                <div className=""></div>
-                {/* CENTER */}
-                <div className="">
-              {/* LINE */}
-              <div className="">
-                {/* LINE CIRCLE */}
-              </div>
-                </div>
-                {/* RIGHT */}
-                <div className="">
-                {/* JOB TITLE */}
-<div className="">Backend Developer</div>
-                {/* JOB DESC */}
-<div className="">My current employment. Way better than the position before!</div>
-                {/* JOB DATE */}
-<div className="">2023 - Present</div>
-                {/* JOB COMPANY */}
-<div className="">NAXTECH</div>
-                </div>
-              </div>
-                {/* EXPERIENCE LIST ITEM */}
-                <div className="">
-                {/* LEFT */}
-                <div className="">
-                {/* JOB TITLE */}
-<div className="">Backend Developer</div>
-                {/* JOB DESC */}
-<div className="">My current employment. Way better than the position before!</div>
-                {/* JOB DATE */}
-<div className="">2023 - Present</div>
-                {/* JOB COMPANY */}
-<div className="">NAXTECH</div>
-                </div>
-
-                {/* CENTER */}
-                <div className="">
-              {/* LINE */}
-              <div className="">
-                {/* LINE CIRCLE */}
-              </div>
-                </div>
-                {/* RIGHT */}
-<div className=""></div>
-              </div>
-            </div>
+        {/* CENTER */}
+        <div className="w-1/6">
+          {/* LINE */}
+          <div className="w-1 h-full bg-gray-600 rounded relative">
+            {/* LINE CIRCLE */}
+            <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
           </div>
         </div>
-        {/* SVG CONTAINER */}
-        <div className="hidden"></div>
+        {/* RIGHT */}
+        <div className="w-1/3"></div>
+      </div>
+      {/* EXPERIENCE LIST ITEM */}
+      <div className="flex justify-between h-48">
+        {/* LEFT */}
+        <div className="w-1/3 ">
+        </div>
+        {/* CENTER */}
+        <div className="w-1/6">
+          {/* LINE */}
+          <div className="w-1 h-full bg-gray-600 rounded relative">
+            {/* LINE CIRCLE */}
+            <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-1/3 ">
+          {/* JOB TITLE */}
+          <div className="p-3 bg-white font-semibold rounded-b-lg rounded-s-lg">Backend Developer</div>
+          {/* JOB DESC */}
+          <div className="p-3 text-sm italic">My current employment. Way better than the position before!</div>
+          {/* JOB DATE */}
+          <div className="p-3 text-red-400 text-sm font-semibold">2023 - Present</div>
+          {/* JOB COMPANY */}
+          <div className="p-1 bg-white rounded  text-sm font-semibold w-fit">NAXTECH</div>
+        </div>
+      </div>
+      <div className="flex justify-between h-48 ">
+        {/* LEFT */}
+        <div className="w-1/3 ">
+           {/* JOB TITLE */}
+           <div className="p-3 bg-white font-semibold rounded-b-lg rounded-s-lg">Backend Developer</div>
+          {/* JOB DESC */}
+          <div className="p-3 text-sm italic">My current employment. Way better than the position before!</div>
+          {/* JOB DATE */}
+          <div className="p-3 text-red-400 text-sm font-semibold">2023 - Present</div>
+          {/* JOB COMPANY */}
+          <div className="p-1 bg-white rounded  text-sm font-semibold w-fit">NAXTECH</div>
+        </div>
+        {/* CENTER */}
+        <div className="w-1/6">
+          {/* LINE */}
+          <div className="w-1 h-full bg-gray-600 rounded relative">
+            {/* LINE CIRCLE */}
+            <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+          </div>
+        </div>
+        {/* RIGHT */}
+        <div className="w-1/3 ">
+         
+        </div>
+      </div>
+    </div>
+  </div>
 
+
+        </div>
       </div>
     </motion.div>
   )
--- src/components/transitionProvider.jsx
+++ src/components/transitionProvider.jsx
@@ -19,7 +19,7 @@ const TransitionProvider = ({ children }) => {
                 <motion.div className="h-screen w-screen fixed bg-black rounded-t-[100px]  bottom-0 z-40 " initial={{ height: "140vh" }}
                     animate={{ height: "0vh", transition: { delay: 0.5 } }} />
                 <div className="h-24" ><Navbar /></div>
-                <div className="h-[calc(100vh-6rem)]">{children}</div>
+                <div className="h-[calc(100vh-6rem)] ">{children}</div>
             </div>
         </AnimatePresence>
     )
